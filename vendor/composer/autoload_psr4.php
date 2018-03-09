5a017d13545f8d688998b8ee80014f1584204141
Coby Tamayo <ctamayo@sitecrafting.com>
2018-03-09 12:05:50 -0800

    change src -> lib for consistency with Timber, Groot

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Conifer\\' => array($baseDir . '/src'),
+    'Conifer\\' => array($baseDir . '/lib'),
 );
