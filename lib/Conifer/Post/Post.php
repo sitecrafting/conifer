eb175638c4ea259cff15fc32849c3c40600f768e
Coby Tamayo <acobster@users.noreply.github.com>
2018-03-19 17:48:40 -0700

    add get_related_by_* API methods
    * get_related_by_taxonomy() method
    * get_related_by_category() and get_related_by_tag() helper methods
    * whitespace, grrr

--- lib/Conifer/Post/Post.php
+++ lib/Conifer/Post/Post.php
@@ -6,6 +6,7 @@
 namespace Conifer\Post;
 
 use Timber\Post as TimberPost;
+use Timber\Term;
 
 use Conifer\Post\Image;
 
@@ -13,19 +14,24 @@ use Conifer\Post\Image;
  * High-level behavior for WP Posts, on top of TimberPost class
  */
 abstract class Post extends TimberPost {
-	use HasTerms;
+  use HasTerms;
+	
+  const RELATED_POST_COUNT = 3;
 
-	/**
-	 * When instantiating TimberImages, create instances of this class
-	 * @var string
-	 */
-	public $ImageClass = '\Conifer\Post\Image';
+  /**
+   * When instantiating TimberImages, create instances of this class
+   * @var string
+   */
+  public $ImageClass = '\Conifer\Post\Image';
 
-	/**
-	 * The default blog landing page URL
-	 * @var string
-	 */
-	protected static $BLOG_URL;
+  /**
+   * The default blog landing page URL
+   * @var string
+   */
+  protected static $BLOG_URL;
+
+  protected $related_by = [];
+  protected $related_post_counts = [];
 
   /**
    * Child classes must declare their own post types
@@ -206,6 +212,76 @@ abstract class Post extends TimberPost {
 
     // return a new instance of the called class
     return new static($id);
+  }
+
+  /**
+   * Get related Posts of the same post type, who share terms in $taxonomy with
+   * this Post.
+   * @param string $taxonomy the taxonomy to associate with, e.g. "category"
+   * @param int $postCount Optional. The number of posts to get. Defaults to 3.
+   * @return Post[] an array of Post objects
+   */
+  public function get_related_by_taxonomy(
+    string $taxonomy,
+    int $postCount = self::RELATED_POST_COUNT
+  ) : array {
+    // Get any previously queried related posts
+    $relatedPosts = $this->related_by[$taxonomy] ?? [];
+    $relatedPostCount = $this->related_post_counts[$taxonomy] ?? null;
+
+    if (count($relatedPosts) < $postCount && !isset($relatedPostCount)) {
+      // There may be more related posts than previously queried; look for them
+      $termIds = array_map(function(Term $term) {
+        return $term->id;
+      }, $this->terms($taxonomy));
+
+      $this->related_by[$taxonomy] = static::get_all([
+        'post_type'     => static::POST_TYPE,
+        'post__not_in'  => [$this->ID],
+        'numberposts'   => $postCount,
+        'tax_query'     => [
+          [
+            'taxonomy'   => $taxonomy,
+            'terms'      => $termIds,
+          ]
+        ]
+      ]);
+
+      if (
+        ($newCount = count($this->related_by[$taxonomy])) < $relatedPostCount
+      ) {
+        // Our query fewer than $postCount posts, so we know this is the
+        // exact number of related posts for this taxonomy. Save this count
+        // for future calls.
+        $this->related_post_counts[$taxonomy] = $newCount;
+      }
+    }
+
+    return $this->related_by[$taxonomy];
+  }
+
+  /**
+   * Get related Posts of the same post type, who share categories with
+   * this Post.
+   * @param int $postCount Optional. The number of posts to get. Defaults to 3.
+   * @return Post[] an array of Post objects
+   */
+  public function get_related_by_category(
+    int $postCount = self::RELATED_POST_COUNT
+  ) : array {
+    return $this->get_related_by_taxonomy('category', $postCount);
+  }
+
+  /**
+   * Get related Posts of the same post type, who share tags with
+   * this Post.
+   * @param int $postCount Optional. The number of posts to get. Defaults to 3.
+   * @return Post[] an array of Post objects
+   */
+  public function get_related_by_tag(
+    int $postCount = self::RELATED_POST_COUNT
+  ) : array {
+    return $this->get_related_by_taxonomy('post_tag', $postCount);
   }
 }
 
