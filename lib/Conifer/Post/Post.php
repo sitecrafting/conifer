1fd3d8da3419433a543428613bd344ff13da0654
Coby Tamayo <ctamayo@sitecrafting.com>
2018-03-30 14:37:41 -0700

    consolidate get_all_grouped_by_term() into HasTerms

--- lib/Conifer/Post/Post.php
+++ lib/Conifer/Post/Post.php
@@ -65,55 +65,6 @@ abstract class Post extends TimberPost {
   }
 
   /**
-   * Get all published posts of this type, grouped by terms of $taxonomy
-   *
-   * @param string $taxonomy the name of the taxonomy to group by,
-   * e.g. "category"
-   * @param array $terms The list of specific terms to filter by.
-   * Defaults to all terms within $taxonomy.
-   * @return an array like:
-   * [
-   *   [ 'term' => { Category 1 WP_Term object }, 'posts' => [...],
-   *   [ 'term' => { Category 2 WP_Term object }, 'posts' => [...],
-   * ]
-   */
-  public static function get_all_grouped_by_term(
-    string $taxonomy,
-    array $terms = []
-  ) : array {
-    // ensure we have a list of taxonomy terms
-    $terms = $terms ?: get_terms(['taxonomy' => $taxonomy]);
-
-    // reduce each term in $taxonomy to an array containing:
-    //  * the term
-    //  * the term's corresponding posts
-    return array_reduce($terms, function(
-      array $grouped,
-      WP_Term $term
-    ) use ($taxonomy) : array {
-      // compose a query for all posts for $category
-      $query = [
-        'post_type' => static::POST_TYPE,
-        'tax_query' => [
-          [
-            'taxonomy' => $taxonomy,
-            'terms'    => $term->term_id,
-          ],
-        ],
-      ];
-
-      // group this term with its respective posts
-      $grouped[] = [
-        'term' => $term,
-        'posts'  => static::get_all($query),
-      ];
-
-      // return the grouped posts so far
-      return $grouped;
-    }, []);
-  }
-
-  /**
    * Get the URL of the blog landing page
    * (what WP calls the "post archive" page)
    *
