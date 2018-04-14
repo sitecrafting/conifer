87a2714eeafebf6b873fac15886f01c4a3aad258
Coby Tamayo <ctamayo@sitecrafting.com>
2018-04-14 14:22:51 -0700

    pending SiteTest case

---
+++ test/cases/SiteTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Test the Conifer\Site class
+ *
+ * @copyright 2018 SiteCrafting, Inc.
+ * @author    Coby Tamayo <ctamayo@sitecrafting.com>
+ */
+
+namespace ConiferTest;
+
+use WP_Mock;
+
+use Conifer\Site;
+
+class SiteTest extends Base {
+  public function test_find_file() {
+    $this->markTestSkipped();
+  }
+
+  public function test_get_assets_version() {
+    $this->markTestSkipped();
+  }
+
+  public function test_theme_file() {
+    $this->markTestSkipped();
+  }
+}
