d5a3809363cd4c7661a8be5e71e7aba5cb25b7ba
Coby Tamayo <acobster@gmail.com>
2018-10-04 08:00:00 -0700

    improve focus of e2e tests

--- cypress/integration/conifer.spec.js
+++ cypress/integration/conifer.spec.js
@@ -17,16 +17,15 @@ describe('Conifer', () => {
       // search for "goo"
       cy.visit('/?s=goo+glue')
 
-      cy.get('.search-results article').should('have.length', 9)
+      cy.get('.search-results article').should('have.length', 8)
       cy.get('article:nth-of-type(1) h2').should('have.text', 'A post about goo glue')
-      cy.get('article:nth-of-type(2) h2').should('have.text', 'A whole post about glue')
-      cy.get('article:nth-of-type(3) h2').should('have.text', 'A whole page about goo')
+      cy.get('article:nth-of-type(2) h2').should('have.text', 'A whole page about goo')
+      cy.get('article:nth-of-type(3) h2').should('have.text', 'Search term double match in meta')
       cy.get('article:nth-of-type(4) h2').should('have.text', 'Search term in content')
       cy.get('article:nth-of-type(5) h2').should('have.text', 'Search term in meta')
       cy.get('article:nth-of-type(6) h2').should('have.text', 'Search term in meta: hello')
-      cy.get('article:nth-of-type(7) h2').should('have.text', 'Search term partial match in meta')
-      cy.get('article:nth-of-type(8) h2').should('have.text', 'Another partial match in meta')
-      cy.get('article:nth-of-type(9) h2').should('have.text', 'A Thing')
+      cy.get('article:nth-of-type(7) h2').should('have.text', 'Another partial match in meta')
+      cy.get('article:nth-of-type(8) h2').should('have.text', 'A Thing')
     })
   })
 
