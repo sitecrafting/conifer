f74b451074b8ffed4cb3a9c1182e3420dd2dcf38
Coby Tamayo <ctamayo@sitecrafting.com>
2018-12-06 12:02:13 -0800

    get Cypress specs passing

--- cypress/integration/conifer.spec.js
+++ cypress/integration/conifer.spec.js
@@ -1,7 +1,8 @@
-/* globals cy, context */
+/* globals cy, context, before */
 describe('Conifer', () => {
   before(() => {
     cy.task('installTheme', 'search-test-theme')
+    cy.task('installFixture', 'meta-search')
   })
 
   context('The home page', () => {
@@ -14,18 +15,25 @@ describe('Conifer', () => {
 
   context('Search', () => {
     it('searches post fields and meta-fields', () => {
-      // search for "goo"
+      // search for "goo glue"
       cy.visit('/?s=goo+glue')
 
       cy.get('.search-results article').should('have.length', 8)
+      // posts with search term in title should come first
       cy.get('article:nth-of-type(1) h2').should('have.text', 'A post about goo glue')
       cy.get('article:nth-of-type(2) h2').should('have.text', 'A whole page about goo')
-      cy.get('article:nth-of-type(3) h2').should('have.text', 'Search term double match in meta')
-      cy.get('article:nth-of-type(4) h2').should('have.text', 'Search term in content')
-      cy.get('article:nth-of-type(5) h2').should('have.text', 'Search term in meta')
-      cy.get('article:nth-of-type(6) h2').should('have.text', 'Search term in meta: hello')
-      cy.get('article:nth-of-type(7) h2').should('have.text', 'Another partial match in meta')
-      cy.get('article:nth-of-type(8) h2').should('have.text', 'A Thing')
+
+      cy.get('.search-results').should('contain', 'Search term double match in meta')
+      cy.get('.search-results').should('contain', 'Search term in content')
+      cy.get('.search-results').should('contain', 'Search term in meta (*bye)')
+      cy.get('.search-results').should('contain', 'Search term in meta (hello)')
+      cy.get('.search-results').should('contain', 'Another partial match in meta')
+      cy.get('.search-results').should('contain', 'A Thing')
+
+      cy.get('.search-results').should('not.contain', 'This shouldn\'t show up at all')
+      cy.get('.search-results').should('not.contain', 'This shouldn\'t either')
+      cy.get('.search-results').should('not.contain', 'Another Thing')
+      cy.get('.search-results').should('not.contain', 'Thing Draft')
     })
   })
 
