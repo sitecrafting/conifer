14177cf472f35fa45d95127b6b42badf9c8ed7b4
Alexander Merk <amerk@Alexanders-MacBook-Pro.local>
2026-07-16 12:03:31 -0700

    feat: adding test for AbstractPolicy

---
+++ test/unit/AbstractPolicyTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Test the Conifer\Authorization\AbstractPolicy class
+ *
+ * @copyright 2026 SiteCrafting, Inc.
+ * @author    Alex Merk <amerk@sitecrafting.com>
+ */
+
+namespace Conifer\Unit;
+
+use Conifer\Authorization\AbstractPolicy;
+use Conifer\Authorization\PolicyInterface;
+
+class AbstractPolicyTest extends Base
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function test_register_returns_instance_of_policy_interface()
+    {
+        $policy = TestPolicy::register();
+
+        $this->assertInstanceOf(PolicyInterface::class, $policy);
+    }
+}
+
+class TestPolicy extends AbstractPolicy
+{
+    public function can_view(): bool
+    {
+        return true;
+    }
+
+    public function adopt(): PolicyInterface
+    {
+        // For testing purposes, just return $this
+        return $this;
+    }
+}
