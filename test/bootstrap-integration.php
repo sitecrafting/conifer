141439609094f43244445dc9e0cc0213ccb3b730
Alexander Merk <amerk@sitecrafting.com>
2026-08-05 15:14:58 -0700

    adding integration test for render() function

--- test/bootstrap-integration.php
+++ test/bootstrap-integration.php
@@ -12,3 +12,5 @@ if (is_dir(__DIR__ . '/wp-tests-lib')) {
   require_once __DIR__ . '/wp-tests-lib/includes/functions.php';
   require_once __DIR__ . '/wp-tests-lib/includes/bootstrap.php';
 }
+
+require_once __DIR__ . '/integration/Base.php';
