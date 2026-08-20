6c6d036533a63b315353ad72775d8057e9fd92ff
Alexander Merk <amerk@sitecrafting.com>
2026-08-19 17:54:02 -0700

    updating documentation for SimpleNotifier

--- lib/Conifer/Notifier/SimpleNotifier.php
+++ lib/Conifer/Notifier/SimpleNotifier.php
@@ -22,6 +22,8 @@
 
 namespace Conifer\Notifier;
 
+use InvalidArgumentException;
+
 /**
  * Class for emailing arbitrary email addresses
  *
@@ -31,6 +33,7 @@ namespace Conifer\Notifier;
  */
 class SimpleNotifier extends EmailNotifier
 {
+  const DEFAULT_EXCEPTION_MESSAGE = 'The $to argument must be a valid email address, a comma-separated list of valid email addresses, or an array of valid email addresses';
   /**
    * The email address(es) to send to
    *
@@ -43,6 +46,8 @@ class SimpleNotifier extends EmailNotifier
    *
    * @param string|array $to the email addresses to send to.
    * Can be a comma-separated string or an array
+   * 
+   * @throws InvalidArgumentException if $to is not a valid single email address, a comma-separated list of valid email addresses, or an array of valid email addresses.
    */
   public function __construct(string|array $to)
   {
@@ -50,7 +55,7 @@ class SimpleNotifier extends EmailNotifier
     $toCopy = $to;
 
     if (empty($to)) {
-      throw new \InvalidArgumentException('The $to argument must be a valid email address, a comma-separated list of valid email addresses, or an array of valid email addresses');
+      throw new \InvalidArgumentException(self::DEFAULT_EXCEPTION_MESSAGE);
     }
 
     if (gettype($to) === 'string') {
@@ -61,17 +66,17 @@ class SimpleNotifier extends EmailNotifier
       if (!empty($toCopy)) {
         foreach ($toCopy as $email) {
           if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            throw new \InvalidArgumentException('The $to argument must be a valid email address, a comma-separated list of valid email addresses, or an array of valid email addresses');
+            throw new \InvalidArgumentException(self::DEFAULT_EXCEPTION_MESSAGE);
           }
         }
         // We are likely working with a single email address, so we will just validate that one
       } else {
         if (!filter_var($to, FILTER_VALIDATE_EMAIL)) {
-          throw new \InvalidArgumentException('The $to argument must be a valid email address, a comma-separated list of valid email addresses, or an array of valid email addresses');
+          throw new \InvalidArgumentException(self::DEFAULT_EXCEPTION_MESSAGE);
         }
       }
     } else if (gettype($to) !== 'array') {
-      throw new \InvalidArgumentException('The $to argument must be a valid email address, a comma-separated list of valid email addresses, or an array of valid email addresses');
+      throw new \InvalidArgumentException(self::DEFAULT_EXCEPTION_MESSAGE);
     }
 
     // If we have reached this point, we have an array of some sort, so we need to validate all of the emails in the array.
@@ -79,7 +84,7 @@ class SimpleNotifier extends EmailNotifier
     if (gettype($toCopy) === 'array') {
       foreach ($toCopy as $email) {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-          throw new \InvalidArgumentException('The $to argument must be a valid email address, a comma-separated list of valid email addresses, or an array of valid email addresses');
+          throw new \InvalidArgumentException(self::DEFAULT_EXCEPTION_MESSAGE);
         }
       }
     }
